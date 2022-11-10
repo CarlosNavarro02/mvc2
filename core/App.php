c05452360a4a10cad3ac00ac11f0ee557cac4155
CarlosNavarro02 <carlos.navarro1793@gmail.com>
2022-11-10 15:53:37 +0000

    a

---
+++ core/App.php
@@ -0,0 +1,42 @@
+<?php
+    /* 
+        Si no la url no especificada ningun controlador (recuros => asigno)
+        uno por defecto => home
+        -Si no la url no especifica ningun metodo => asigno por defecto : index  */
+
+    class App  
+    {
+        function __construct() {
+            if (isset($_GET["url"]) && !empty($_GET["url"])) {
+                $url = $_GET["url"];
+            }else {
+                $url = "home";
+            }
+            // /product/show -> producto: recurso ; show : accion
+            $arguments = explode('/',trim($url,'/'));
+            $controllerName = array_shift($arguments); //product : ProductController
+            $controllerName =  ucwords($controllerName) . "Controller";
+            if (count($arguments)) {
+                $method = array_shift($arguments);
+            }else {
+                $method = "index";
+            }
+            //Voy a cargar el contrlolador 
+            $file = "app/controllers/$controllerName" . ".php";
+            if (file_exists($file)) {
+                require_once $file;
+            }else {
+                http_response_code(404);
+                die("No encontrado");
+            }
+            $controllerObject = new $controllerName();
+            if (method_exists($controllerObject,$method)) {
+                $controllerObject->$method($arguments);
+            }else {
+                http_response_code(404);
+                die("No encontrado");
+            }
+        }
+        
+    }
+    
--- start.php
+++ start.php
@@ -5,24 +5,25 @@
         recurso: controladores
         accion: metodos del controladores
         parametros : find -> id de producto */
-    require_once "../Controller.php";   
+    require_once "core/App.php";
+    $app = new App();   
 
-    $app = new Controller();
+    /*$app = new Controller();
     //defino variable de peticion en la url
 
-    /* -1 Recoger el metodo que pasan como parametro y di no
-    especifican ninguna carga el metodo home */
+     -1 Recoger el metodo que pasan como parametro y di no
+    especifican ninguna carga el metodo home 
     if (isset($_GET["method"])) {
         $method = $_GET["method"];
     }else {
         $method = "home";
     }
 
-    /* -2 Verificar que el metodo introducido existe */
+    /* -2 Verificar que el metodo introducido existe 
     if (method_exists($app, $method)) {
         $app->$method();
     }else {
         http_response_code(404);
         die("Metodo no encontrado");
     }
-
+*/
